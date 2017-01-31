cd12fddf5d73955f3fe0b396c66e64b86b6fd13b
David <dk.shin1992@gmail.com>
2017-01-31 16:18:41 +1100

    order bragg amp params like shots in analysis data

--- main_fwm_rabi.m
+++ main_fwm_rabi.m
@@ -16,6 +16,15 @@ verbose=2;
 % end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% vars to save to output
+vars_save={'path_config','path_log',...
+    'exp_config',...
+    'TXY','files_out',...
+    'box_cent','box_hwidth','box_edge',...
+    'txy_scat_mode','pop_scat_mode',...
+    'exp_id','bragg_amp'...
+    };
+
 %% Main
 % load configuration file
 run(path_config);
@@ -53,13 +62,18 @@ end
 
 % Capture counts in each scat order
 txy_scat_mode=cell(nShot,4);
+pop_scat_mode=zeros(nShot,4);
 for i_shot=1:nShot
     for i_ord=1:4
+        % capture counts in set box
         txy_scat_mode{i_shot,i_ord}=boxcull(TXY{i_shot},box_edge{i_ord});
+        
+        % evaluate population in each mode
+        pop_scat_mode(i_shot,i_ord)=size(txy_scat_mode{i_shot,i_ord},1);
     end
 end
 
-%% Plot captured counts
+%%% Plot captured counts
 colors='rgbk';
 h_scat_modes=figure();
 hold on;
@@ -73,3 +87,37 @@ xlabel('X [m]'); ylabel('Y [m]'); zlabel('T [s]');
 fname_str='scat_modes';
 saveas(h_scat_modes,[configs.files.dirout,fname_str,'.fig']);
 saveas(h_scat_modes,[configs.files.dirout,fname_str,'.png']);
+
+%% Match shots with Bragg amplitude
+% exp_config.id/bragg_amp <==> files_out.id_ok
+% Create ordered file# to log index table
+exp_id=zeros(max(exp_config.id),1);
+counter=1;
+for i=1:length(exp_config.id)
+    exp_id(exp_config.id(i))=counter;
+    counter=counter+1;
+end
+
+% Create matching ordered list of Bragg amplitudes
+bragg_amp=zeros(size(nShot,1));
+for i=1:nShot
+    file_id=files_out.id_ok(i);     % file ID for this shot
+    ind_log=exp_id(file_id);
+    if ind_log==0   % check if Bragg amplitude was recorded for this shot
+        warning('Bragg amplitude for file #%d is missing. Setting to NaN.',file_id);
+        bragg_amp(i)=NaN;
+    else
+        bragg_amp(i)=exp_config.bragg_amp(ind_log);
+    end
+end
+
+
+
+%% Save data
+for i = 1:length(vars_save)
+    if ~exist(vars_save{i},'var')
+        warning(['Variable "',vars_save{i},'" does not exist.']);
+        continue;
+    end
+    save(configs.files.saveddata,vars_save{i},'-v6','-append');     % -v6 version much faster (but no compression)?
+end
